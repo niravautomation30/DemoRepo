ad32273c1a0c2f6845c7ff7894d9e99892aed39a
Nirav Maisuria <nirav.30890@gmail.com>
2020-04-29 13:45:26 +0530

    my first git commit for my selenium project

---
+++ src/test/java/com/qa/tests/DemoTest1.java
@@ -0,0 +1,40 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	
+	@Test
+	public void sum() {
+		System.out.println("in sum");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(a+b, 30);
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("in sub");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(b-a, 10);
+	}
+	
+	@Test
+	public void div() {
+		System.out.println("in div");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(b/a, 2);
+	}
+	
+	@Test
+	public void mul() {
+		System.out.println("in mul");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(a*b, 200);
+	}
+
+}
