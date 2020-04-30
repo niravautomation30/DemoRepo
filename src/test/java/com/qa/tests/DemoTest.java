ea2f335712536f49f5caf504831b38c46d44e257
Nirav Maisuria <nirav.30890@gmail.com>
2020-04-30 15:22:25 +0530

    launch Firefox Browser

--- src/test/java/com/qa/tests/DemoTest.java
+++ src/test/java/com/qa/tests/DemoTest.java
@@ -3,10 +3,18 @@
  */
 package com.qa.tests;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DemoTest {
+	
+	public WebDriver driver;
+	public String driverPath = "/home/nirav/Documents/selenium jar/geckodriver-v0.26.0-linux64/geckodriver";
 	
 	@Test
 	public void sum() {
@@ -38,6 +46,25 @@ public class DemoTest {
 		int a=10;
 		int b=20;
 		Assert.assertEquals(a*b, 200);
+	}
+	
+	
+	@Test
+	public void openBrow() {
+//		System.setProperty("webdriver.firefox.marionette", "/home/nirav/Documents/selenium jar/geckodriver-v0.26.0-linux64/geckodriver");
+		//System.setProperty("webdriver.chrome.driver", "/home/nirav/Documents/selenium jar/chrome/chromedriver");		
+//		driver = new FirefoxDriver();
+//		driver = new ChromeDriver();
+//		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+//		driver.get("https://www.guru99.com/first-webdriver-script.html");
+		
+		System.setProperty("webdriver.gecko.driver", driverPath);
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("marionette", true);
+        driver = new FirefoxDriver(capabilities);		
+        driver.get("http://demo.guru99.com/selenium/guru99home/");
+        driver.quit();
+
 	}
 
 }
